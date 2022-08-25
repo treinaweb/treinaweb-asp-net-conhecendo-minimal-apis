43d8deb75fee544282385c8cbc90cc65997257c2
Cleyson Lima <cleysonph@gmail.com>
2022-08-25 20:55:46 -0300

    02:03 - entendendo como funciona o sistema de rotas

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Olá TreinaWeb!");
+app.MapGet("/", () => "Requisição feita com o verbo GET");
+app.MapPost("/", () => "Requisição feita com o verbo POST");
+app.MapPut("/", () => "Requisição feita com o verbo PUT");
+app.MapDelete("/", () => "Requisição feita com o verbo DELETE");
+app.MapMethods("/", new[] { "PATCH", "OPTIONS" }, () => "Requisição feita com o verbo PATCH ou OPTIONS");
 
 app.Run();
