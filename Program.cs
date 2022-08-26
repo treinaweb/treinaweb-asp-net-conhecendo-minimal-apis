4a30e38925b8b11e7a32843eac0feebe0a25cd54
Cleyson Lima <cleysonph@gmail.com>
2022-08-25 21:18:41 -0300

    02:04 - recebendo dados no corpo da requisição

--- Program.cs
+++ Program.cs
@@ -2,9 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Requisição feita com o verbo GET");
-app.MapPost("/", () => "Requisição feita com o verbo POST");
+app.MapPost("/", (Product product) =>
+{
+    Console.WriteLine(product);
+    return "Requisição feita com o verbo POST";
+});
 app.MapPut("/", () => "Requisição feita com o verbo PUT");
 app.MapDelete("/", () => "Requisição feita com o verbo DELETE");
 app.MapMethods("/", new[] { "PATCH", "OPTIONS" }, () => "Requisição feita com o verbo PATCH ou OPTIONS");
 
 app.Run();
+
+record Product(string Name, string Description, decimal Price);
