5b41928f0163b9598743230a3a8caf64dcf9cc7d
Cleyson Lima <cleysonph@gmail.com>
2022-08-25 22:44:26 -0300

    03:10 - criando a rota de cadastro de Tarefas

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 using TWTodos.Data.Contexts;
+using TWTodos.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<TWTodoContext>();
 
 var app = builder.Build();
+
+app.MapPost("/api/todos", (Todo todo, TWTodoContext context) =>
+{
+    context.Todos.Add(todo);
+    context.SaveChanges();
+    return Results.Created($"/api/todos/{todo.Id}", todo);
+});
 
 app.Run();
